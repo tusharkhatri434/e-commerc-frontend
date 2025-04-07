719efec7d041c435e0efdac2e9124aea959de13e
tusharkhatri434 <tusharkhatr8193@gmail.com>
2025-04-07 20:10:13 +0530

    Login Form UI

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,7 +36,7 @@ const NavBar = () => {
       <img src={assets.search_icon} alt="" className="w-5 cursor-pointer" />
         
         <div className="group relative">
-          <img className="w-5 cursor-pointer" src={assets.profile_icon} />
+          <NavLink to="/login"><img className="w-5 cursor-pointer" src={assets.profile_icon} /></NavLink>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 px-5 bg-slate-100 text-gray-700">
               <p className="cursor-pointer hover:text-black">My profile</p>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+  const [isLoginForm,setLoginForm] = useState(true);
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPasssword] = useState("");
+  
+  const navigate = useNavigate();
+
+  const formTypeChangeHandler = ()=>{
+    setEmail('');
+    setPasssword('');
+    setName('');
+    setLoginForm(!isLoginForm);
+     }
+
+  const formSubmitHandler = (e)=>{
+    e.preventDefault();
+    if(isLoginForm){
+      if(email && password){
+        console.log(email,password);
+        navigate("/");
+      }
+    }
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={formSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+    <div className="inline-flex items-center gap-2 mb-2 mt-10">
+       <p className="prata-regular text-3xl">{isLoginForm ? "Login" : "Sign Up"}</p>
+       <hr className="border-none h-[1.5px] w-8 bg-gray-800"/>
+    </div>
+    { (!isLoginForm) ? 
+    <input onChange={(e)=>setName(e.target.value)} type="text" className="w-full px-3 py-2 border border-gray-800" placeholder="Name" required={true}  value={name} /> : null }
+    <input onChange={(e)=>setEmail(e.target.value)} type="email" className="w-full px-3 py-2 border border-gray-800" placeholder="Email" required={true}  value={email}/>
+    <input onChange={(e)=>setPasssword(e.target.value)} type="password" className="w-full px-3 py-2 border border-gray-800" placeholder="Password" required={true} value={password} />
+    <div className="w-full flex justify-between text-sm mt-[-8px]">
+       <p className="cursor-pointer">Forgot your password?</p>
+       <p onClick={formTypeChangeHandler} className="cursor-pointer">{isLoginForm?"Create account":"Login Here"}</p>
+    </div>
+    <button type="submit" className="bg-black text-white font-light px-8 py-2 mt-4">{isLoginForm ? "Sign In" : "Sign Up"}</button>
+ </form>
   )
 }
 
